d35c7e1bd92e7e1df5094221d2ed3ee60e906f4b
Stefan Oltmann <github@stefan-oltmann.de>
2024-09-26 00:14:14 +0200

    Show a proper Work in progress hint.

--- app/src/commonMain/kotlin/App.kt
+++ app/src/commonMain/kotlin/App.kt
@@ -2,11 +2,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.unit.IntSize
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @OptIn(ExperimentalResourceApi::class)
@@ -14,18 +12,12 @@ import org.jetbrains.compose.resources.ExperimentalResourceApi
 fun App() {
     MaterialTheme {
 
-        var currentSize by remember { mutableStateOf(IntSize(0, 0)) }
-
         Column(
-            Modifier
-                .fillMaxSize()
-                .onSizeChanged {
-                    currentSize = it
-                },
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize(),
         ) {
 
-            Text("Current size: " + currentSize)
+            Text("The ONI Seed Browser is a work in progress!")
 
         }
     }
