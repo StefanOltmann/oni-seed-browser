6e63fd7827e29691a179ac69345e7f50707ebfca
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-03 10:08:48 +0200

    Refactored

--- app/src/commonMain/kotlin/ui/GeysersView.kt
+++ app/src/commonMain/kotlin/ui/GeysersView.kt
@@ -19,35 +19,13 @@
 
 package ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import model.Geyser
 import model.GeyserType
-import org.jetbrains.compose.resources.painterResource
-import theme.DefaultSpacer
-import theme.darkGreen
-import theme.darkRed
 import theme.defaultPadding
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -67,56 +45,10 @@ fun ColumnScope.GeysersView(geysers: List<Geyser>) {
 
             val count = geysersCountOfStarter[geyserType] ?: 0
 
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .padding(4.dp)
-                    .width(250.dp)
-                    .background(
-                        color = MaterialTheme.colorScheme.surfaceVariant,
-                        shape = RoundedCornerShape(16.dp)
-                    )
-            ) {
-
-                Image(
-                    painter = painterResource(getGeyserDrawable(geyserType)),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(32.dp)
-                        .clip(CircleShape)
-                        .scale(1.2f)
-                )
-
-                DefaultSpacer()
-
-                Text(
-                    text = geyserType.displayName,
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    modifier = Modifier.weight(1F)
-                )
-
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .size(26.dp)
-                        .background(
-                            color = if (count > 0) darkGreen else darkRed,
-                            shape = CircleShape
-                        )
-                ) {
-
-                    Text(
-                        text = count.toString(),
-                        style = MaterialTheme.typography.bodyLarge,
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.onBackground,
-                        modifier = Modifier.offset(y = -2.dp)
-                    )
-                }
-
-                Spacer(modifier = Modifier.width(4.dp))
-            }
+            GeyserCountAndName(
+                geyserType,
+                count
+            )
         }
     }
 }
