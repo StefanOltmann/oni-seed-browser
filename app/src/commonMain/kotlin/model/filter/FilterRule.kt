b7ac2e74d4b4f89f77d923555970c943009cb2e5
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-03 16:09:43 +0200

    Designed filter object

---
+++ app/src/commonMain/kotlin/model/filter/FilterRule.kt
@@ -0,0 +1,37 @@
+/*
+ * ONI Seed Browser
+ * Copyright (C) 2024 Stefan Oltmann
+ * https://stefan-oltmann.de/oni-seed-browser
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package model.filter
+
+import model.AsteroidType
+
+data class FilterRule(
+
+    /* NULL = All asteroids */
+    val asteroidId: AsteroidType?,
+
+    /* Items */
+    val geyserCount: FilterItemGeyserCount?,
+    val geyserOutput: FilterItemGeyserOutput?,
+    val worldTrait: FilterItemWorldTrait?,
+    val spaceDestinationOutput: FilterItemSpaceDestinationOutput?,
+
+    /* OR-connected rules (only to be set for the root rules) */
+    val orRules: List<FilterRule>
+)
