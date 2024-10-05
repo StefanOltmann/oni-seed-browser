eb76b99f52d1477666dcd5b83e68775dfc3630f1
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-05 22:55:28 +0200

    Cluster: Added a mapping to asteroid types

--- app/src/commonMain/kotlin/model/Cluster.kt
+++ app/src/commonMain/kotlin/model/Cluster.kt
@@ -25,187 +25,437 @@ import kotlinx.serialization.Serializable
 @Serializable
 enum class Cluster(
     val prefix: String,
-    val displayName: String
+    val displayName: String,
+    val asteroidTypes: List<AsteroidType>
 ) {
 
     /** Terra */
     BASE_TERRA(
         prefix = "SNDST-A",
-        displayName = "Terra"
+        displayName = "Terra",
+        asteroidTypes = listOf(
+            AsteroidType.SandstoneDefault
+        )
     ),
 
     /** Ceres */
     BASE_CERES(
         prefix = "CER-A",
-        displayName = "Ceres"
+        displayName = "Ceres",
+        asteroidTypes = listOf(
+            AsteroidType.CeresBaseGameAsteroid
+        )
     ),
 
     /** Oceania */
     BASE_OCEANIA(
         prefix = "OCAN-A",
-        displayName = "Oceania"
+        displayName = "Oceania",
+        asteroidTypes = listOf(
+            AsteroidType.Oceania
+        )
     ),
 
     /** Rime */
     BASE_RIME(
         prefix = "S-FRZ",
-        displayName = "Rime"
+        displayName = "Rime",
+        asteroidTypes = listOf(
+            AsteroidType.SandstoneFrozen
+        )
     ),
 
     /** Verdante */
     BASE_VERDANTE(
         prefix = "LUSH-A",
-        displayName = "Verdante"
+        displayName = "Verdante",
+        asteroidTypes = listOf(
+            AsteroidType.ForestLush
+        )
     ),
 
     /** Arboria */
     BASE_ARBORIA(
         prefix = "FRST-A",
-        displayName = "Arboria"
+        displayName = "Arboria",
+        asteroidTypes = listOf(
+            AsteroidType.ForestDefault
+        )
     ),
 
     /** Volcanea */
     BASE_VOLCANEA(
         prefix = "VOLCA",
-        displayName = "Volcanea"
+        displayName = "Volcanea",
+        asteroidTypes = listOf(
+            AsteroidType.Volcanic
+        )
     ),
 
     /** The Badlands */
     BASE_THE_BADLANDS(
         prefix = "BAD-A",
-        displayName = "The Badlands"
+        displayName = "The Badlands",
+        asteroidTypes = listOf(
+            AsteroidType.Badlands
+        )
     ),
 
     /** Aridio */
     BASE_ARIDIO(
         prefix = "HTFST-A",
-        displayName = "Aridio"
+        displayName = "Aridio",
+        asteroidTypes = listOf(
+            AsteroidType.ForestHot
+        )
     ),
 
     /** Oasisse */
     BASE_OASISSE(
         prefix = "OASIS-A",
-        displayName = "Oasisse"
+        displayName = "Oasisse",
+        asteroidTypes = listOf(
+            AsteroidType.Oasis
+        )
     ),
 
     /** Terra */
     DLC_TERRA(
         prefix = "V-SNDST-C",
-        displayName = "Terra"
+        displayName = "Terra",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaSandstoneDefault,
+            AsteroidType.MediumRadioactiveVanillaWarpPlanet,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Ceres */
     DLC_CERES(
         prefix = "V-CER-C",
-        displayName = "Ceres"
+        displayName = "Ceres",
+        asteroidTypes = listOf(
+            AsteroidType.CeresClassicAsteroid,
+            AsteroidType.MediumSwampy,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Oceania */
     DLC_OCEANIA(
         prefix = "V-OCAN-C",
-        displayName = "Oceania"
+        displayName = "Oceania",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaOceania,
+            AsteroidType.MediumForestyWasteland,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Squelchy */
     DLC_SQUELCHY(
         prefix = "V-SWMP-C",
-        displayName = "Squelchy"
+        displayName = "Squelchy",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaSwampDefault,
+            AsteroidType.MediumForestyRadioactiveVanillaWarpPlanet,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Rime */
     DLC_RIME(
         prefix = "V-SFRZ-C",
-        displayName = "Rime"
+        displayName = "Rime",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaSandstoneFrozen,
+            AsteroidType.MediumSwampy,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Verdante */
     DLC_VERDANTE(
         prefix = "V-LUSH-C",
-        displayName = "Verdante"
+        displayName = "Verdante",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaForestDefault,
+            AsteroidType.MediumSandyRadioactiveVanillaWarpPlanet,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Arboria */
     DLC_ARBORIA(
         prefix = "V-FRST-C",
-        displayName = "Arboria"
+        displayName = "Arboria",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaArboria,
+            AsteroidType.MediumSandyRadioactiveVanillaWarpPlanet,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Volcanea */
     DLC_VOLCANEA(
         prefix = "V-VOLCA-C",
-        displayName = "Volcanea"
+        displayName = "Volcanea",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaVolcanic,
+            AsteroidType.MediumRadioactiveVanillaWarpPlanet,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** The Badlands */
     DLC_THE_BADLANDS(
         prefix = "V-BAD-C",
-        displayName = "The Badlands"
+        displayName = "The Badlands",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaBadlands,
+            AsteroidType.MediumRadioactiveVanillaWarpPlanet,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Aridio */
     DLC_ARIDIO(
         prefix = "V-HTFST-C",
-        displayName = "Aridio"
+        displayName = "Aridio",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaAridio,
+            AsteroidType.MediumSandySwamp,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Oasisse */
     DLC_OASISSE(
         prefix = "V-OASIS-C",
-        displayName = "Oasisse"
+        displayName = "Oasisse",
+        asteroidTypes = listOf(
+            AsteroidType.VanillaOasis,
+            AsteroidType.MediumRadioactiveVanillaWarpPlanet,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.MiniRegolithMoonlet
+        )
     ),
 
     /** Terrania */
     DLC_TERRANIA(
         prefix = "SNDST-C",
-        displayName = "Terrania"
+        displayName = "Terrania",
+        asteroidTypes = listOf(
+            AsteroidType.TerraMoonlet,
+            AsteroidType.IdealLandingSite,
+            AsteroidType.WarpOilySwamp,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     ),
 
     /** Ceres Minor */
     DLC_CERES_MINOR(
         prefix = "CER-C",
-        displayName = "Ceres Minor"
+        displayName = "Ceres Minor",
+        asteroidTypes = listOf(
+            AsteroidType.CeresSpacedOutAsteroid,
+            AsteroidType.SwampyLandingSite,
+            AsteroidType.OilRichWarpTarget,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     ),
 
     /** Folia */
     DLC_FOLIA(
         prefix = "FRST-C",
-        displayName = "Folia"
+        displayName = "Folia",
+        asteroidTypes = listOf(
+            AsteroidType.ForestMoonlet,
+            AsteroidType.SwampyLandingSite,
+            AsteroidType.OilRichWarpTarget,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     ),
 
     /** Quagmiris */
     DLC_QUAGMIRIS(
         prefix = "SWMP-C",
-        displayName = "Quagmiris"
+        displayName = "Quagmiris",
+        asteroidTypes = listOf(
+            AsteroidType.SwampMoonlet,
+            AsteroidType.MetalHeavyLandingSite,
+            AsteroidType.OilRichWarpTarget,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     ),
 
     /** Metallic Swampy Moonlet */
     DLC_METALLIC_SWAMPY_MOONLET(
         prefix = "M-SWMP-C",
-        displayName = "Metallic Swampy Moonlet"
+        displayName = "Metallic Swampy Moonlet",
+        asteroidTypes = listOf(
+            AsteroidType.MiniBadlands,
+            AsteroidType.MiniMetallicSwampyStart,
+            AsteroidType.MiniForestFrozenWarp,
+            AsteroidType.MiniFlipped,
+            AsteroidType.MiniRadioactiveOcean,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     ),
 
     /** The Desolands Moonlet */
     DLC_THE_DESOLANDS_MOONLET(
         prefix = "M-BAD-C",
-        displayName = "The Desolands Moonlet"
+        displayName = "The Desolands Moonlet",
+        asteroidTypes = listOf(
+            AsteroidType.MiniBadlandsStart,
+            AsteroidType.MiniRadioactiveOceanWarp,
+            AsteroidType.MiniMetallicSwampy,
+            AsteroidType.MiniForestFrozen,
+            AsteroidType.MiniFlipped,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     ),
 
     /** Frozen Forest Moonlet */
     DLC_FROZEN_FOREST_MOONLET(
         prefix = "M-FRZ-C",
-        displayName = "Frozen Forest Moonlet"
+        displayName = "Frozen Forest Moonlet",
+        asteroidTypes = listOf(
+            AsteroidType.MiniBadlandsWarp,
+            AsteroidType.MiniForestFrozenStart,
+            AsteroidType.MiniMetallicSwampy,
+            AsteroidType.MiniFlipped,
+            AsteroidType.MiniRadioactiveOcean,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     ),
 
     /** Flipped Moonlet */
     DLC_FLIPPED_MOONLET(
         prefix = "M-FLIP-C",
-        displayName = "Flipped Moonlet"
+        displayName = "Flipped Moonlet",
+        asteroidTypes = listOf(
+            AsteroidType.MiniBadlandsWarp,
+            AsteroidType.MiniFlippedStart,
+            AsteroidType.MiniMetallicSwampy,
+            AsteroidType.MiniForestFrozen,
+            AsteroidType.MiniRadioactiveOcean,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     ),
 
     /** Radioactive Ocean Moonlet */
     DLC_RADIOACTIVE_OCEAN_MOONLET(
         prefix = "M-RAD-C",
-        displayName = "Radioactive Ocean Moonlet"
+        displayName = "Radioactive Ocean Moonlet",
+        asteroidTypes = listOf(
+            AsteroidType.MiniBadlands,
+            AsteroidType.MiniRadioactiveOceanStart,
+            AsteroidType.MiniFlippedWarp,
+            AsteroidType.MiniMetallicSwampy,
+            AsteroidType.MiniForestFrozen,
+            AsteroidType.TundraMoonlet,
+            AsteroidType.MarshyMoonlet,
+            AsteroidType.MooMoonlet,
+            AsteroidType.WaterMoonlet,
+            AsteroidType.NiobiumMoonlet,
+            AsteroidType.RegolithMoonlet
+        )
     );
 
     fun isFrostyPlanet(): Boolean =
