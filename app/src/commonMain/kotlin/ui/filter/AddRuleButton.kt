a0bf3afd29ae5dce41e08073bf890a6e0d8e0ab0
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-10 10:40:08 +0200

    Extracted AddRuleButton.kt

---
+++ app/src/commonMain/kotlin/ui/filter/AddRuleButton.kt
@@ -0,0 +1,91 @@
+/*
+ * ONI Seed Browser
+ * Copyright (C) 2024 Stefan Oltmann
+ * https://stefan-oltmann.de/oni-seed-browser
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ui.filter
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import ui.noRippleClickable
+import ui.onHover
+import ui.theme.halfSpacing
+import ui.theme.hoverColor
+
+@Composable
+fun AddRuleButton(
+    text: String,
+    onClick: () -> Unit
+) {
+
+    val fontColor = MaterialTheme.colorScheme.onBackground
+
+    val hovered = remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(halfSpacing, Alignment.CenterHorizontally),
+        modifier = Modifier
+            .onHover(hovered)
+            .drawBehind {
+
+                val dashLength = 8.dp.toPx()
+                val gapLength = 8.dp.toPx()
+                val strokeWidth = 2.dp.toPx()
+
+                drawRoundRect(
+                    color = fontColor,
+                    size = this.size,
+                    cornerRadius = CornerRadius(
+                        8.dp.toPx(),
+                        8.dp.toPx()
+                    ),
+                    style = Stroke(
+                        width = strokeWidth,
+                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(dashLength, gapLength))
+                    )
+                )
+            }
+            .size(120.dp, 40.dp)
+            .noRippleClickable(onClick)
+    ) {
+
+        Text(
+            text = "ADD '$text' RULE",
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold,
+            color = if (hovered.value)
+                hoverColor
+            else
+                MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
--- app/src/commonMain/kotlin/ui/filter/RuleEditor.kt
+++ app/src/commonMain/kotlin/ui/filter/RuleEditor.kt
@@ -19,38 +19,25 @@
 
 package ui.filter
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import model.filter.FilterQuery
 import ui.HorizontalSeparator
-import ui.noRippleClickable
-import ui.onHover
 import ui.theme.FillSpacer
 import ui.theme.defaultPadding
 import ui.theme.defaultSpacing
-import ui.theme.halfSpacing
-import ui.theme.hoverColor
 
 @Composable
 fun RuleEditor(
@@ -178,53 +165,5 @@ private fun RuleEditorRow(
     }
 }
 
-@Composable
-private fun AddRuleButton(
-    text: String,
-    onClick: () -> Unit
-) {
 
-    val fontColor = MaterialTheme.colorScheme.onBackground
-
-    val hovered = remember { mutableStateOf(false) }
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(halfSpacing, Alignment.CenterHorizontally),
-        modifier = Modifier
-            .onHover(hovered)
-            .drawBehind {
-
-                val dashLength = 8.dp.toPx()
-                val gapLength = 8.dp.toPx()
-                val strokeWidth = 2.dp.toPx()
-
-                drawRoundRect(
-                    color = fontColor,
-                    size = this.size,
-                    cornerRadius = CornerRadius(
-                        8.dp.toPx(),
-                        8.dp.toPx()
-                    ),
-                    style = Stroke(
-                        width = strokeWidth,
-                        pathEffect = PathEffect.dashPathEffect(floatArrayOf(dashLength, gapLength))
-                    )
-                )
-            }
-            .size(120.dp, 40.dp)
-            .noRippleClickable(onClick)
-    ) {
-
-        Text(
-            text = "ADD '$text' RULE",
-            style = MaterialTheme.typography.bodyLarge,
-            fontWeight = FontWeight.Bold,
-            color = if (hovered.value)
-                hoverColor
-            else
-                MaterialTheme.colorScheme.onBackground
-        )
-    }
-}
 
