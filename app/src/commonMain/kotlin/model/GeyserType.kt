e6813c6a7935bfc3031faf68ca8dfb8c51a877a6
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-13 20:42:20 +0200

    Rated geysers

--- app/src/commonMain/kotlin/model/GeyserType.kt
+++ app/src/commonMain/kotlin/model/GeyserType.kt
@@ -25,7 +25,8 @@ import kotlinx.serialization.Serializable
 @Serializable
 enum class GeyserType(
     val type: String,
-    val displayName: String
+    val displayName: String,
+    rating: Rating = Rating.NEUTRAL
 ) {
 
     /*
@@ -34,63 +35,93 @@ enum class GeyserType(
 
     COOL_STEAM(
         type = "steam",
-        displayName = "Cool Steam Vent"
+        displayName = "Cool Steam Vent",
+        /* Good thing to have for water production. */
+        rating = Rating.GOOD
     ),
     HYDROGEN(
         type = "hot_hydrogen",
-        displayName = "Hydrogen Vent"
+        displayName = "Hydrogen Vent",
+        /* Free energy */
+        rating = Rating.VERY_GOOD
     ),
     NATURAL_GAS(
         type = "methane",
-        displayName = "Natural Gas Geyser"
+        displayName = "Natural Gas Geyser",
+        /* Free energy & for cooking */
+        rating = Rating.VERY_GOOD
     ),
     CHLORINE(
         type = "chlorine_gas",
-        displayName = "Chlorine Gas Vent"
+        displayName = "Chlorine Gas Vent",
+        /* Has its uses */
+        rating = Rating.GOOD
     ),
     HOT_STEAM(
         type = "hot_steam",
-        displayName = "Steam Vent"
+        displayName = "Steam Vent",
+        /* Usable energy, harder to handle. */
+        rating = Rating.GOOD
     ),
     HOT_CO2(
         type = "hot_co2",
-        displayName = "Carbon Dioxide Vent"
+        displayName = "Carbon Dioxide Vent",
+        /* Mostly useless */
+        rating = Rating.VERY_BAD
     ),
     HOT_POLLUTED_O2(
         type = "hot_po2",
-        displayName = "Hot Polluted Oxygen Vent"
+        displayName = "Hot Polluted Oxygen Vent",
+        /* Crap */
+        rating = Rating.VERY_BAD
     ),
     INFECTIOUS_POLLUTED_O2(
         type = "slimy_po2",
-        displayName = "Infectious Polluted Oxygen Vent"
+        displayName = "Infectious Polluted Oxygen Vent",
+        /* Usually does not produce enough to be helpful. */
+        rating = Rating.BAD
     ),
     WATER(
         type = "hot_water",
-        displayName = "Water Geyser"
+        displayName = "Water Geyser",
+        /* We love free water */
+        rating = Rating.VERY_GOOD
     ),
     COOL_SLUSH_WATER(
         type = "slush_water",
-        displayName = "Cool Slush Geyser"
+        displayName = "Cool Slush Geyser",
+        /* Cool polluted water, free or germs. */
+        rating = Rating.VERY_GOOD
     ),
     POLLUTED_WATER(
         type = "filthy_water",
-        displayName = "Polluted Water Vent"
+        displayName = "Polluted Water Vent",
+        /* Many plants can directly consume it. */
+        rating = Rating.VERY_GOOD
     ),
     COOL_SALT_WATER(
         type = "slush_salt_water",
-        displayName = "Cool Salt Slush Geyser"
+        displayName = "Cool Salt Slush Geyser",
+        /* Brine brings coolness and salt. */
+        rating = Rating.VERY_GOOD
     ),
     HOT_SALT_WATER(
         type = "salt_water",
-        displayName = "Salt Water Geyser"
+        displayName = "Salt Water Geyser",
+        /* It's useful */
+        rating = Rating.GOOD
     ),
     LIQUID_CO2(
         type = "liquid_co2",
-        displayName = "Carbon Dioxide Geyser"
+        displayName = "Carbon Dioxide Geyser",
+        /* Source of coolness, but heats up very fast. */
+        rating = Rating.NEUTRAL
     ),
     LEAKY_OIL_FISSURE(
         type = "oil_drip",
-        displayName = "Leaky Oil Fissure"
+        displayName = "Leaky Oil Fissure",
+        /* Doesn't matter if it's there. Does not produce a lot most of the times. */
+        rating = Rating.NEUTRAL
     ),
     SULFUR_GEYSER(
         type = "liquid_sulfur",
@@ -98,42 +129,62 @@ enum class GeyserType(
     ),
     IRON_VOLCANO(
         type = "molten_iron",
-        displayName = "Iron Volcano"
+        displayName = "Iron Volcano",
+        /* Metal geysers are good. */
+        rating = Rating.GOOD
     ),
     COPPER_VOLCANO(
         type = "molten_copper",
-        displayName = "Copper Volcano"
+        displayName = "Copper Volcano",
+        /* Metal geysers are good. */
+        rating = Rating.GOOD
     ),
     GOLD_VOLCANO(
         type = "molten_gold",
-        displayName = "Gold Volcano"
+        displayName = "Gold Volcano",
+        /* Metal geysers are good. */
+        rating = Rating.GOOD
     ),
     ALUMINIUM_VOLCANO(
         type = "molten_aluminum",
-        displayName = "Aluminium Volcano"
+        displayName = "Aluminium Volcano",
+        /* Metal geysers are good. */
+        rating = Rating.GOOD
     ),
     COBALT_VOLCANO(
         type = "molten_cobalt",
-        displayName = "Cobalt Volcano"
+        displayName = "Cobalt Volcano",
+        /* Metal geysers are good. */
+        rating = Rating.GOOD
     ),
     TUNGSTEN_VOLCANO(
         type = "molten_tungsten",
-        displayName = "Tungsten Volcano"
+        displayName = "Tungsten Volcano",
+        /* Metal geysers are good. */
+        rating = Rating.GOOD
     ),
     NIOBIUM_VOLCANO(
         type = "molten_niobium",
-        displayName = "Niobium Volcano"
+        displayName = "Niobium Volcano",
+        /* Metal geysers are good. */
+        rating = Rating.GOOD
     ),
     VOLCANO(
         type = "big_volcano",
-        displayName = "Valcano"
+        displayName = "Valcano",
+        /* Free energy & stone */
+        rating = Rating.GOOD
     ),
     MINOR_VOLCANO(
         type = "small_volcano",
-        displayName = "Minor Volcano"
+        displayName = "Minor Volcano",
+        /* Free energy & stone */
+        rating = Rating.GOOD
     ),
     OIL_RESERVOIR(
         type = "OilWell",
-        displayName = "Oil Reservoir"
+        displayName = "Oil Reservoir",
+        /* Rated good, because the more the better. */
+        rating = Rating.GOOD
     )
 }
