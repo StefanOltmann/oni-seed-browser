3ca29d4edf7f73177f9a788308c774612b186982
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-13 20:31:37 +0200

    Added Rating enum

---
+++ app/src/commonMain/kotlin/model/Rating.kt
@@ -0,0 +1,29 @@
+/*
+ * ONI Seed Browser
+ * Copyright (C) 2024 Stefan Oltmann
+ * https://stefan-oltmann.de/oni-seed-browser
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package model
+
+enum class Rating(value: Int) {
+
+    VERY_GOOD(2),
+    GOOD(1),
+    NEUTRAL(0),
+    BAD(-1),
+    VERY_BAD(-2)
+}
