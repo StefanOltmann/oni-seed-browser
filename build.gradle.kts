66e5a762d971ccf000d42db8243f8a42459bf92b
Stefan Oltmann <github@stefan-oltmann.de>
2024-10-22 22:59:45 +0200

    Activated Detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,20 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.pluginCompose) apply false
+    alias(libs.plugins.detekt)
+}
+
+apply(plugin = "io.gitlab.arturbosch.detekt")
+
+detekt {
+    source.from("app/src", "build.gradle.kts")
+    allRules = true
+    config.setFrom("$projectDir/detekt.yml")
+    parallel = true
+    ignoreFailures = true
+    autoCorrect = true
+}
+
+dependencies {
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${libs.versions.detekt.get()}")
 }
