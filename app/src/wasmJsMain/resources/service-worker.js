c49482738848ae1bf57e8cf4c64c365ddfc37d92
Stefan Oltmann <github@stefan-oltmann.de>
2025-06-12 00:03:55 +0200

    Increased cache version to force updates

--- app/src/wasmJsMain/resources/service-worker.js
+++ app/src/wasmJsMain/resources/service-worker.js
@@ -2,7 +2,7 @@
  * Service Worker for ONI Seed Browser app
  */
 
-const CACHE_NAME = 'oni-seed-browser-cache-v4';
+const CACHE_NAME = 'oni-seed-browser-cache-v5';
 
 const ASSETS_TO_CACHE = [
     './',
