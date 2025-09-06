4e214cf450bcbc7aec8ba3c584d3cc77c6ce52ca
Stefan Oltmann <github@stefan-oltmann.de>
2025-09-06 16:39:05 +0200

    Bumped cache version

--- app/src/wasmJsMain/resources/service-worker.js
+++ app/src/wasmJsMain/resources/service-worker.js
@@ -2,7 +2,7 @@
  * Service Worker for ONI Seed Browser app
  */
 
-const CACHE_NAME = 'oni-seed-browser-cache-v6';
+const CACHE_NAME = 'oni-seed-browser-cache-v7';
 
 const ASSETS_TO_CACHE = [
     './',
