9c2e4562736dbcfdcb5c48f2123cd0c63b919d2d
Stefan Oltmann <github@stefan-oltmann.de>
2025-09-07 17:40:26 +0200

    sync with backend object model

--- app/src/commonMain/kotlin/model/UploadMetadata.kt
+++ app/src/commonMain/kotlin/model/UploadMetadata.kt
@@ -3,10 +3,11 @@ package model.server
 import kotlinx.serialization.Serializable
 
 /**
- * This is the info we store into the database about the upload
+ * This is the info we store into the database about the upload.
+ * This is data we may need for analytics on bad map reports.
  */
 @Serializable
-data class UploadDatabase(
+data class UploadMetadata(
 
     val userId: String,
 
