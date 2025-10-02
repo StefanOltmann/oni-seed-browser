a7a7d546fd9c8d5efa332e08b04ae20dbea00c40
Stefan Oltmann <github@stefan-oltmann.de>
2025-10-02 20:05:42 +0200

    Lib updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -242,13 +242,3 @@ project.afterEvaluate {
     }
 }
 // endregion
-
-// region Work around temporary Compose bugs.
-configurations.all {
-    attributes {
-        // https://github.com/JetBrains/compose-jb/issues/1404#issuecomment-1146894731
-        attribute(Attribute.of("ui", String::class.java), "awt")
-    }
-}
-// endregion
-
