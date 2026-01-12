626e8decfd50d8f7e247b6e34b62ee3e52623497
Stefan Oltmann <github@stefan-oltmann.de>
2026-01-12 17:36:56 +0100

    Added imprint & privacy policy to the web interface

--- app/src/commonMain/kotlin/ui/Footer.kt
+++ app/src/commonMain/kotlin/ui/Footer.kt
@@ -24,20 +24,30 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import io.github.stefanoltmann.app.generated.resources.Res
+import io.github.stefanoltmann.app.generated.resources.uiImprint
+import io.github.stefanoltmann.app.generated.resources.uiPrivacyPolicy
+import io.ktor.util.PlatformUtils
+import org.jetbrains.compose.resources.stringResource
 import ui.icons.FooterGraphic
+import ui.theme.DefaultSpacer
+import ui.theme.DoubleSpacer
 
 @Composable
 fun Footer() {
@@ -69,11 +79,46 @@ fun Footer() {
 
         Text(
             text = APP_VERSION,
-            color = Color.White.copy(alpha = 0.3f),
+            color = Color.White.copy(alpha = 0.2f),
             fontSize = 12.sp,
-            fontWeight = FontWeight.Bold,
             fontFamily = FontFamily.Monospace,
-            modifier = Modifier.align(Alignment.CenterEnd).padding(end = 6.dp)
+            modifier = Modifier.align(Alignment.CenterStart).padding(start = 6.dp)
         )
+
+        if (PlatformUtils.IS_BROWSER) {
+
+            Row(
+                modifier = Modifier.align(Alignment.CenterEnd).padding(end = 6.dp)
+            ) {
+
+                Text(
+                    text = stringResource(Res.string.uiImprint),
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    modifier = Modifier.noRippleClickable {
+
+                        if (Locale.current.language == "de")
+                            uriHandler.openUri("https://stefan-oltmann.de/portfolio/de/imprint.html")
+                        else
+                            uriHandler.openUri("https://stefan-oltmann.de/portfolio/en/imprint.html")
+                    }
+                )
+
+                DefaultSpacer()
+
+                Text(
+                    text = stringResource(Res.string.uiPrivacyPolicy),
+                    color = Color.White,
+                    fontSize = 12.sp,
+                    modifier = Modifier.noRippleClickable {
+
+                        if (Locale.current.language == "de")
+                            uriHandler.openUri("privacy.de.html")
+                        else
+                            uriHandler.openUri("privacy.en.html")
+                    }
+                )
+            }
+        }
     }
 }
